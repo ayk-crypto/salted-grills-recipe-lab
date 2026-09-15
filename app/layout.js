1f75b4dbf41aa4af5ee13c617bba7497248309b2
Asfand Yar <76555129+ayk-crypto@users.noreply.github.com>
2026-09-16 00:11:56 +0500

    Mount costing conversion controls

--- app/layout.js
+++ app/layout.js
@@ -11,6 +11,7 @@ import CostControlUX from "./CostControlUX";
 import ShelfSenseOperations from "./ShelfSenseOperations";
 import IngredientPriceShelfSense from "./IngredientPriceShelfSense";
 import IngredientPurchaseDisplay from "./IngredientPurchaseDisplay";
+import CostingConversionUX from "./CostingConversionUX";
 import AppInputFocusGuard from "./AppInputFocusGuard";
 import SearchFilters from "./SearchFilters";
 import FlagControls from "./FlagControls";
@@ -24,4 +25,4 @@ export const metadata={
  icons:{icon:"/icon.svg",apple:"/icon.svg"}
 };
 export const viewport={width:"device-width",initialScale:1,viewportFit:"cover",themeColor:"#11181a"};
-export default function RootLayout({children}){return <html lang="en"><body className={manrope.variable}><AppInputFocusGuard/><MenuImportActions/><BulkMenuActions/><SafetyConfirmation/><CostControlUX/><ShelfSenseOperations/><IngredientPriceShelfSense/><IngredientPurchaseDisplay/><FlagControls/><SearchFilters/>{children}</body></html>}
+export default function RootLayout({children}){return <html lang="en"><body className={manrope.variable}><AppInputFocusGuard/><MenuImportActions/><BulkMenuActions/><SafetyConfirmation/><CostControlUX/><ShelfSenseOperations/><IngredientPriceShelfSense/><IngredientPurchaseDisplay/><CostingConversionUX/><FlagControls/><SearchFilters/>{children}</body></html>}
